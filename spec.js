5b39c7ce8d2eef9f0a09538254fac1908f85d6c0
JessicaHerford <jrherford12@gmail.com>
2016-05-15 10:02:21 -0600

    first commit

---
+++ spec.js
@@ -0,0 +1,21 @@
+var origFn = browser.driver.controlFlow().execute;
+
+browser.driver.controlFlow().execute = function() {
+  var args = arguments;
+
+  // queue 100ms wait
+  origFn.call(browser.driver.controlFlow(), function() {
+    return protractor.promise.delayed(100);
+  });
+
+  return origFn.apply(browser.driver.controlFlow(), args);
+};
+
+describe('mapquest usertest', function(){
+
+  it('should do something', funciton(){
+    browser.get('https://www.mapquest.com/');
+    
+  })
+
+})
